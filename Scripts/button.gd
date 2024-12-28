db44126d9fb456692909272e9bf56bd40f6a0543
durrapickle <quinnob0707@gmail.com>
2024-12-28 17:53:04 +1100

    test commit

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -2,6 +2,7 @@ extends Button
 
 @export var next_level: int = 1
 
+# HELLO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
