fbc740a6815e592d6324210147960ad3f6d77d44
Iamhere <95515863+Iamhere345@users.noreply.github.com>
2024-12-28 22:19:47 +1100

    add dialogue stuff

---
+++ Scripts/person.gd
@@ -0,0 +1,16 @@
+extends TextureButton
+
+@export var dialogue_box: DialogueBox
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	dialogue_box.start("START_TEST")
