dd1943924a00cc1ce7db2d91360607771852f51a
Iamhere <95515863+Iamhere345@users.noreply.github.com>
2024-12-30 23:50:54 +1100

    fully playable

--- Scripts/Level_Scripts/final_boat.gd
+++ Scripts/Level_Scripts/final_boat.gd
@@ -4,8 +4,8 @@ extends Node2D
 @onready var ending_fade = $Control/Ending
 @onready var win = $Win
 
-var scores = ["95280 B+", "11297 A", "138952 A+++", "4227 C", "617 D-"]
-var playstles = ["Koala", "Reindeer", "Hare", "Turtle", "PlayStation® 3™", "Cyclist", "Guy outside your window", "Madeline from Celeste", "Guy from dinner tonight"]
+var scores = ["95280 B+", "11297 A-", "138952 A+++", "4227 C-", "617 D-"]
+var playstles = ["Koala", "Reindeer", "PlayStation® 3™", "Cyclist", "Guy outside your window", "Madeline from Celeste", "Guy from dinner tonight"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,4 +16,4 @@ func _on_boat_pressed() -> void:
 	ending_fade.fade_out()
 	await ending_fade.next_pressed
 	
-	win.game_over("Score: " + scores.pick_random() + "\n Playstyle: " + scores.pick_random())
+	win.game_over("Score: " + scores.pick_random() + "\n Playstyle: " + playstles.pick_random())
